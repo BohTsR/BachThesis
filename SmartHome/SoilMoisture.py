8b4b618326ecf807b9a45900ee7caec4450783c5
BohTsR <160582711+BohTsR@users.noreply.github.com>
2024-05-03 17:08:00 +0200

    Update SoilMoisture.py

--- SmartHome/SoilMoisture.py
+++ SmartHome/SoilMoisture.py
@@ -1,32 +1,3 @@
-#from machine import Pin, ADC
-#import time
-
-#analogPin = ADC(Pin(4))
-#digitalPin = Pin(35, Pin.IN)
-
-
-#analogPinValue = analogPin.read()
-#digitalPinValue = digitalPin.value()
-
-#THRESHOLD = 1000 
-                
-#while True:
-#    value = analogPin.read()  # Read the analog value from the sensor
-#    moisture =  (100 - ((value/4095.00) * 100 ))
-#    print(value)
-#    time.sleep_ms(1000) 
-
-
-
-
-
-
-#print(analogPinValue)
-#print(digitalPinValue)
-
-
-
-
 from machine import Pin, SoftI2C
 from machine import Pin, ADC
 import time
@@ -42,7 +13,6 @@ soil.atten(ADC.ATTN_11DB)       #Full range: 3.3v
 soil.width(ADC.WIDTH_12BIT)     #range 0 to 4095
 
 # START
-text = 'Starting...'
 
 while True:
     try:
@@ -51,10 +21,6 @@ while True:
         m = (max_moisture-soil.read())*100/(max_moisture-min_moisture)
         moisture = '{:.1f} %'.format(m)
         print('Soil Moisture:', moisture)
-        lcd.clear()
-        lcd.putstr('Soil Moisture: ')
-        lcd.move_to(0,1)
-        lcd.putstr(moisture)
         time.sleep(5)
     except:
         pass
